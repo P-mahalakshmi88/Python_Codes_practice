438691375d12b0e71a4603477c0526b915d7e652
P-mahalakshmi88 <143864715+P-mahalakshmi88@users.noreply.github.com>
2025-02-02 20:39:46 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,9 @@
+#Janmansh and Assignments
+# cook your dish here
+for _ in range(int(input())):
+    x = int(input())
+    
+    if (x + 3) <= 10 :
+        print('Yes')
+    else:
+        print('No')
