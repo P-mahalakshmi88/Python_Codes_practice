6b4bcb101682d85ed3a4c5d43ebf2df8b643f27a
P-mahalakshmi88 <143864715+P-mahalakshmi88@users.noreply.github.com>
2025-02-25 19:08:17 +0530

    Add files via upload

---
+++ 26.py
@@ -0,0 +1,10 @@
+# Good Program
+
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if ( x % 4 == 0):
+        print('Good')
+    else:
+        print('Not Good')
